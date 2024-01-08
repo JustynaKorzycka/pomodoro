d3f119cd7e3b2eb20d26ecf99f0a804a5afc8350
Justyna <justyna.korzycka@gmail.com>
2024-01-08 23:37:43 +0100

    create timerData file with name and types of timers

---
+++ src/data/timerData.js
@@ -0,0 +1,7 @@
+const TIMER_DATA = [
+ { type: "pomodoro", name: "pomodoro" },
+ { type: "shortBreak", name: "short break" },
+ { type: "longBreak", name: "long break" },
+];
+
+export default TIMER_DATA;
